bca98118305823f43efd466056e2734f7ed5d4f9
Shivangi Singh <shivangisingh0103@gmail.com>
2023-09-17 11:47:12 +0530

    first updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="shivangi",
+    author_email="shivangisingh0103@gmail.com",
+    install_requires=["pandas","numpy","Flask"],
+    packages=find_packages()
+)
