41007c01cfab700efbbc59390461fc0c2743b9e6
Shivangi Singh <shivangisingh0103@gmail.com>
2023-09-17 12:00:37 +0530

    file updated

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 from setuptools import setup, find_packages
+from typing import List
 
+HYPEN_E_DOT='-e .'
+
+
+def get_requiremet(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        
+    return requirements
 
 setup(
     name="src",
     version="0.0.1",
     author="shivangi",
     author_email="shivangisingh0103@gmail.com",
-    install_requires=["pandas","numpy","Flask"],
+    install_requires=get_requiremet("requirements.txt"),
     packages=find_packages()
 )
